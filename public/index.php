a1234891fbd119642c7e80fdeb1f96e5967f4f92
gyarmex <gyarmex@gmail.com>
2025-03-16 14:50:44 +0900

    試しに表示されるように修正

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+// require __DIR__ . '/../vendor/autoload.php';
+// $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+
 
 // ローカルでアプリを使用したい場合以下をコメントアウト解除
 // use Illuminate\Contracts\Http\Kernel;
@@ -23,10 +25,12 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 // $user = env('DB_USERNAME');
 // $pass = env('DB_PASSWORD');
 
+echo ("hello");
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
 
-$kernel = $app->make(Kernel::class);
-$response = $kernel->handle(Request::capture())->send();
-$kernel->terminate(Request::capture(), $response);
+// use Illuminate\Contracts\Http\Kernel;
+// use Illuminate\Http\Request;
+
+// $kernel = $app->make(Kernel::class);
+// $response = $kernel->handle(Request::capture())->send();
+// $kernel->terminate(Request::capture(), $response);
