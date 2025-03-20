c39e1822d88e50b57e0ebd4e420111c9922a7182
gyarmex <gyarmex@gmail.com>
2025-03-20 09:14:42 +0900

    publicフォルダ修正

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Route::get('/', function () {
+    return 'Laravel動作テストOK！';
+});
