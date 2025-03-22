8f44c74a520320640ee9730dbc5d303436fb0f86
gyarmex <gyarmex@gmail.com>
2025-03-22 15:44:36 +0900

    controllerまできているか確認

--- app/Http/PostController.php
+++ app/Http/PostController.php
@@ -9,25 +9,26 @@ class PostController extends Controller
 {
     public function index()
     {
-        return 'PostControllerのindexは動いています';
+        dd('Controllerまで来てます');
+        // return 'PostControllerのindexは動いています';
         // $posts = DB::table('user_table')->orderBy('created_at', 'desc')->get();
         // return view('posts.index', compact('posts'));
     }
 
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'task' => 'required|string',
-        ]);
+    // public function store(Request $request)
+    // {
+    //     $request->validate([
+    //         'name' => 'required|string|max:255',
+    //         'task' => 'required|string',
+    //     ]);
 
-        DB::table('user_table')->insert([
-            'name' => $request->name,
-            'task' => $request->task,
-            'completed' => 0,
-            'created_at' => now(),
-        ]);
+    //     DB::table('user_table')->insert([
+    //         'name' => $request->name,
+    //         'task' => $request->task,
+    //         'completed' => 0,
+    //         'created_at' => now(),
+    //     ]);
 
-        return redirect('/');
-    }
+    //     return redirect('/');
+    // }
 }
